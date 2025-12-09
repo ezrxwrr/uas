f594d6920917505c90fba286af3fa52cff54d0ce
kiman <rifqirahman2525@gmail.com>
2025-12-09 07:54:01 +0700

    update

--- projekuas.sql
+++ projekuas.sql
@@ -1,194 +1,138 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.2
 -- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Dec 01, 2025 at 02:04 AM
--- Server version: 8.4.3
--- PHP Version: 8.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `projekuas`
---
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `donasi`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `donasi` (
   `donation_id` int UNSIGNED NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
   `payment_id` int UNSIGNED NOT NULL,
   `nominal` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `pembayaran`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `pembayaran` (
   `payment_id` int UNSIGNED NOT NULL,
   `donation_id` int UNSIGNED NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
-  `metode` enum('transfer bank','E-Wallet','QRIS') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `metode` enum('transfer bank','E-Wallet','QRIS') 
+      CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `reward`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `reward` (
   `reward_id` int UNSIGNED NOT NULL,
   `nama` varchar(255) NOT NULL,
   `badge` enum('Gold','Silver','Bronze') NOT NULL,
   `total_donasi` int UNSIGNED NOT NULL,
-  `transaksi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `transaksi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `users`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `users` (
   `user_id` int UNSIGNED NOT NULL,
   `nama` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `user_reward`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `user_reward` (
   `user_reward_id` int UNSIGNED NOT NULL,
   `reward_id` int UNSIGNED NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
   `donation_id` int UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
+-- Indexes
+-- --------------------------------------------------------
 
---
--- Indexes for table `donasi`
---
 ALTER TABLE `donasi`
   ADD PRIMARY KEY (`donation_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `payment_id` (`payment_id`);
 
---
--- Indexes for table `pembayaran`
---
 ALTER TABLE `pembayaran`
   ADD PRIMARY KEY (`payment_id`),
   ADD KEY `donation_id` (`donation_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `reward`
---
 ALTER TABLE `reward`
   ADD PRIMARY KEY (`reward_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `nama` (`nama`,`email`);
 
---
--- Indexes for table `user_reward`
---
 ALTER TABLE `user_reward`
   ADD PRIMARY KEY (`user_reward_id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `donation_id` (`donation_id`),
   ADD KEY `reward_id` (`reward_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+-- --------------------------------------------------------
+-- AUTO_INCREMENT Set
+-- --------------------------------------------------------
 
---
--- AUTO_INCREMENT for table `donasi`
---
 ALTER TABLE `donasi`
   MODIFY `donation_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `pembayaran`
---
 ALTER TABLE `pembayaran`
   MODIFY `payment_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `reward`
---
 ALTER TABLE `reward`
   MODIFY `reward_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `user_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `user_reward`
---
 ALTER TABLE `user_reward`
   MODIFY `user_reward_id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
+-- --------------------------------------------------------
+-- Constraints
+-- --------------------------------------------------------
 
---
--- Constraints for table `donasi`
---
 ALTER TABLE `donasi`
-  ADD CONSTRAINT `donasi_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `donasi_ibfk_1` FOREIGN KEY (`user_id`)
+      REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `pembayaran`
---
 ALTER TABLE `pembayaran`
-  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`donation_id`) REFERENCES `donasi` (`donation_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`user_id`)
+      REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`donation_id`)
+      REFERENCES `donasi` (`donation_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `user_reward`
---
 ALTER TABLE `user_reward`
-  ADD CONSTRAINT `user_reward_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_reward_ibfk_2` FOREIGN KEY (`donation_id`) REFERENCES `donasi` (`donation_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user_reward_ibfk_3` FOREIGN KEY (`reward_id`) REFERENCES `reward` (`reward_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+  ADD CONSTRAINT `user_reward_ibfk_1` FOREIGN KEY (`user_id`)
+      REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_reward_ibfk_2` FOREIGN KEY (`donation_id`)
+      REFERENCES `donasi` (`donation_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_reward_ibfk_3` FOREIGN KEY (`reward_id`)
+      REFERENCES `reward` (`reward_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
